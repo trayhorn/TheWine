d60c1fc43e97af6d26bc678396e30b040f10503a
Ivan Amoshyi <amoshyiivan@gmail.com>
2023-04-05 20:57:10 +0300

    pop up

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,15 @@
 const modalButtonEl = document.querySelector('#modal-button');
 const backdropEl = document.querySelector('#backdrop');
 const closeIconEl = document.querySelector('#close-modal');
+const popUpEl = document.querySelector('.pop-up');
+const popUpIcon = document.querySelector('#close-popUp');
 
 function openModal() {
-  backdropEl.classList.add('is-open');
+  if (window.innerWidth >= 767) {
+    backdropEl.classList.add('is-open');
+  } else {
+    popUpEl.classList.add('visible');
+  }
 }
 function closeModal() {
   backdropEl.classList.remove('is-open');
@@ -11,4 +17,7 @@ function closeModal() {
 
 modalButtonEl.addEventListener('click', openModal);
 closeIconEl.addEventListener('click', closeModal);
+popUpIcon.addEventListener('click', () => {
+  popUpEl.classList.remove('visible');
+});
 
