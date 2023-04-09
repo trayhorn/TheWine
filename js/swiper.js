23806a59312b19616c2c961da49a944739e579b5
Ivan Amoshyi <amoshyiivan@gmail.com>
2023-04-09 21:25:16 +0300

    slider fixes

--- js/swiper.js
+++ js/swiper.js
@@ -2,8 +2,13 @@ new Swiper('.swiper', {
   loop: true,
   slidesPerView: 1,
   spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
   breakpoints: {
     768: {
+      loop: false,
       slidesPerView: 2,
       spaceBetween: 30,
     },
@@ -12,8 +17,8 @@ new Swiper('.swiper', {
       spaceBetween: 40,
     },
   },
-  // navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
